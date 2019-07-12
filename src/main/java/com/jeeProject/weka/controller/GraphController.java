f161dc9b0438700d9cce94cec974e2595fd3ac65
Bartholome <bartholome.lebruno@gmail.com>
2019-07-12 13:57:38 +0200

    preparing graph generator

---
+++ src/main/java/com/jeeProject/weka/controller/GraphController.java
@@ -0,0 +1,56 @@
+package com.jeeProject.weka.controller;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.image.WritableImage;
+import javafx.stage.Stage;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class GraphController {
+
+    public void start(Stage stage) {
+        stage.setTitle("Line Chart Sample");
+        //defining the axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Number of Month");
+        xAxis.setLabel("Number of Month");
+        //creating the chart
+        LineChart<Number, Number> lineChart =
+                new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Stock Monitoring, 2010");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        //populating the series with data
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(2, 14));
+        series.getData().add(new XYChart.Data(3, 15));
+        series.getData().add(new XYChart.Data(4, 24));
+        series.getData().add(new XYChart.Data(5, 34));
+        Scene scene = new Scene(lineChart, 800, 600);
+        lineChart.setAnimated(false);
+        lineChart.getData().add(series);
+        saveAsPng(scene, "../image.png");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void saveAsPng(Scene scene, String path) {
+        WritableImage image = scene.snapshot(null);
+        File file = new File(path);
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
