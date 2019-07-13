1f08f9aaa1ab804b273c8f5841afbe809eb299f9
Bartholome <bartholome.lebruno@gmail.com>
2019-07-13 12:25:59 +0200

    s

--- src/main/java/com/jeeProject/weka/controller/UserController.java
+++ src/main/java/com/jeeProject/weka/controller/UserController.java
@@ -9,7 +9,9 @@ import com.jeeProject.weka.repository.UserRepository;
 import com.jeeProject.weka.service.TokenHandlerService;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.sql.Timestamp;
